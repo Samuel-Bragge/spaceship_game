235b2d9a6b8ea860622b3a8396f2afa7cd8cea7d
Justin Chang <jqchang@Justins-MacBook-Pro.local>
2017-03-12 15:52:19 -0700

    background horizontal scrolling take 2

--- spaceships/Background.swift
+++ spaceships/Background.swift
@@ -5,7 +5,7 @@ class Background: SKSpriteNode {
     // how fast the background should move past.
     // 0 is full adjustment, no movement as the world goes past
     // 1 is no adjustment, background passes at normal speed
-    var movementMultiplier = CGFloat(0)
+    var movementMultiplier = CGFloat(1)
     // jumpAdjustment will store how many points of x position
     // this background has jumped forward, useful for calculating
     // future seamless jump points:
